f91aefd81c6fe28cd5f80bf0147469e52554733b
Warren Carvajal <wcarvajal17@gmail.com>
2018-11-13 12:03:47 -0600

    migrated to react native app

--- src/app/Models/airport.model.ts
+++
@@ -1,5 +0,0 @@
-export interface Airport {
-    name: string,
-    city: string,
-    countryName: string
-}
--- src/app/global.ts
+++
@@ -1,6 +0,0 @@
-export var GLOBAL = {
-	url: 'https://laravel-api-ionic.herokuapp.com/api',
-	statsApi: 'https://api.flightstats.com/flex/',
-	appId: '44cee9e5',
-	appKey: '8a2ebed0d4d47e7005a060f5f20fa118'
-};
